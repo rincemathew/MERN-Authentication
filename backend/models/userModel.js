dbec2e8574ea344e4b0b4932936cc8e12a970f82
Rince Mathew <rincemathewmanattu@gmail.com>
2024-08-07 16:00:14 +0530

    generate token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,6 +18,16 @@ const userSchema = mongoose.Schema(
     },
   },
   {
+    //he timestamps option is used to automatically add createdAt and updatedAt fields to your schema. This feature is particularly useful for tracking the creation and update times of documents.
+// {
+//   _id: 60c72b2f5f4b5a3c4c8e8b9e,
+//   name: 'John Doe',
+//   email: 'john.doe@example.com',
+//   createdAt: 2024-07-26T12:34:56.789Z,
+//   updatedAt: 2024-07-26T12:34:56.789Z,
+//   __v: 0
+// }
+
     timestamps: true,
   }
 );
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 Mern stack authentication that sotres a JWT in an HTTP-Only cookie. It also uses Redux Toolkit and the React Bootstrap library.
 
-44:59
+1:00:00
+
+<!-- https://github.com/bradtraversy/mern-auth/blob/master/backend/config/db.js -->
