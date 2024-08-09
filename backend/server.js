22f2c6664a7ee8a56297396a773c1c8f9d493e5d
Rince Mathew <rincemathewmanattu@gmail.com>
2024-08-09 14:28:19 +0530

    backend config

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,36 @@
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRouter.js'
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
+connectDB();
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api/users',userRoutes);
 
-app.get('/',(req,res)=>{
-    res.send('server is ready')
-})
-
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
 app.use(notFound);
 app.use(errorHandler)
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 Mern stack authentication that sotres a JWT in an HTTP-Only cookie. It also uses Redux Toolkit and the React Bootstrap library.
 
-1:10:00
+1:50:00
 
 <!-- https://github.com/bradtraversy/mern-auth/blob/master/backend/config/db.js -->
